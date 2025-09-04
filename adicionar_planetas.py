542ae84562514d8b014049dd8aec5115a89e2ba5
Lorenadalmeida <llorenaliv07@gmail.com>
2025-09-04 22:12:35 +0000

    salvar

---
+++ adicionar_planetas.py
@@ -0,0 +1,16 @@
+planetas = [] 
+def adicionar_planeta():
+    nome = input("digite o nome do planeta:" )
+    tamanho = float(input("digite o tamanho do planeta (em km):"))
+    distancia = float(input("digite a distancia do sol(emm milhões de km):"))
+
+    planeta = {
+        "nome": nome,
+        "tamanho": tamanho,
+        "distancia": distancia 
+        }  
+    planetas.append(planeta)
+    print(f"\n planeta '{nome}) registrado com sucesso!")
+   
+    adicionar_planeta()
+    print(planetas)
